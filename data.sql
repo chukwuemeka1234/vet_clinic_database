279bf0d5cd336c70e2066a59c92291f4aa118735
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-03-02 23:53:11 +0100

    Query and Update animals table

--- data.sql
+++ data.sql
@@ -12,3 +12,24 @@ VALUES ('Pikachu', '2021-01-07', 15.04, false, 1);
 INSERT INTO animals (name, date_of_birth, weight_kg, neutered, 
 escape_attempts)
 VALUES ('Devimon', '2017-05-12', 11.00, true, 5);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Charmander', '2020-02-08', 0, false, 11.00);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Plantmon', '2021-11-15', 2, true, 5.7);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Squirtle', '1993-04-02', 3, false, 12.13);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Angemon', '2005-06-12', 1, true, 45.00);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Boarmon', '2005-06-07', 7, true, 20.40);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Blossom', '1998-10-13', 3, true, 17.00);
+
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES('Ditto', '2022-05-14', 4, true, 22.00);
--- queries.sql
+++ queries.sql
@@ -23,3 +23,110 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 -- Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+--Begin transaction
+--First transaction
+BEGIN TRANSACTION;
+
+--update the animals table by setting the species column to unspecified
+UPDATE animals
+SET species = 'unspecified';
+
+SELECT species from animals;
+
+--roll back the change and verify that species columns went back to the state before transaction.
+ROLLBACK TRANSACTION;
+
+SELECT species from animals;
+
+--SECOND TRANSACTION
+BEGIN TRANSACTION;
+
+--Update the animals table by setting the species column to digimon for all animals that have a name ending in mon
+UPDATE animals
+SET
+     species = 'digimon'
+WHERE
+     name LIKE '%mon';
+
+--Update the animals table by setting the species column to pokemon for all animals that don't have species already set.
+UPDATE animals
+SET
+     species = 'pokemon'
+WHERE
+     species IS NULL;
+
+     COMMIT;
+
+SELECT species FROM animals;
+
+-- Inside a transaction delete all records in the animals table
+TRUNCATE TABLE animals;
+
+SELECT * FROM animals;
+
+--then roll back the transaction
+ROLLBACK TRANSACTION;
+
+SELECT * FROM animals;
+
+--Inside a transaction Delete all animals born after Jan 1st, 2022
+BEGIN TRANSACTION;
+
+DELETE
+     FROM
+         animals
+    WHERE
+         date_of_birth > '2022-01-01';
+
+--Create a savepoint for the transaction.
+SAVEPOINT SP1;
+
+--Update all animals' weight to be their weight multiplied by -1.
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+--Rollback to the savepoint
+ROLLBACK TO SP1;
+
+--Commit transaction
+COMMIT;
+
+--queries to answer the following questions:
+
+--How many animals are there?
+SELECT
+COUNT(name)
+FROM
+animals;
+
+--How many animals have never tried to escape?
+SELECT
+COUNT(escape_attempts)
+FROM animals
+WHERE escape_attempts=0;
+
+--What is the average weight of animals?
+SELECT AVG(weight_kg)
+FROM animals;
+
+--Who escapes the most, neutered or not neutered animals?
+SELECT neutered,
+SUM(escape_attempts)
+FROM animals
+GROUP BY neutered;
+
+--What is the minimum and maximum weight of each type of animal?
+SELECT species,
+MIN(weight_kg) as MINweight, MAX(weight_kg) as MAXweight
+FROM animals
+GROUP BY species;
+
+--What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species,
+AVG(escape_attempts)
+FROM
+     animals
+WHERE
+     date_of_birth < '2000-01-01' AND date_of_birth > '1990-01-01' 
+GROUP BY species;
